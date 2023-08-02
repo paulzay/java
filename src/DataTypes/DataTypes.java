dfac422997587cbce8e8c81ea6ce878fcc9091aa
Paul Ogolla <paulotieno2@gmail.com>
2023-08-02 16:32:29 +0300

    learn about data types

---
+++ src/DataTypes/DataTypes.java
@@ -0,0 +1,19 @@
+package DataTypes;
+
+public class DataTypes {
+    // Strings
+    String name = "Paul";
+
+    // Numbers/Integers
+    int age = 3;
+
+    // Boolean
+    boolean fat = true;
+
+    // Float
+    double height = 172.3;
+
+    //constants
+    final String actor = "Daniel";
+
+}
