6923e9bc57adb83eb3671e562af175b6bb531818
Paul Ogolla <paulotieno2@gmail.com>
2023-08-03 15:56:16 +0300

    learn primitive and reference types

--- src/DataTypes/DataTypes.java
+++ src/DataTypes/DataTypes.java
@@ -1,5 +1,7 @@
 package DataTypes;
 
+import java.util.Date;
+
 public class DataTypes {
     // Strings
     String name = "Paul";
@@ -15,5 +17,22 @@ public class DataTypes {
 
     //constants
     final String actor = "Daniel";
+
+    //date type
+    Date date = new Date();
+
+    // primitive types
+    byte largeByte = -128;
+    short theInt = 8989;
+    int myInt = 88989889;
+    long theLong = 78784783748375L;
+
+    // fresh one
+    float pi = 3.14F;
+    double doublePi = 3.12456;
+    char a = 'A'; //takes a single character
+
+    // reference types
+//    String numero = new String(original: "Maya");
 
 }
