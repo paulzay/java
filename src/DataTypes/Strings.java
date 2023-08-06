de315d0b4b6a962eaf6599d9c3fdc739eeb7a873
Paul Ogolla <paulotieno2@gmail.com>
2023-08-06 22:07:33 +0300

    more on string manipulation

--- src/DataTypes/Strings.java
+++ src/DataTypes/Strings.java
@@ -1,2 +1,12 @@
-package DataTypes;public class Strings {
+package DataTypes;
+
+public class Strings {
+    public static void main(String[] args){
+        String name = "Paul";
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.charAt(2));
+        System.out.println(name.contains("ul"));
+    }
+
 }
