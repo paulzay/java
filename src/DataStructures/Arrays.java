99b59b1ae265a8777c9227dfedbe956991a1fbbd
Paul Ogolla <paulotieno2@gmail.com>
2023-08-06 22:11:56 +0300

    learn arrays and their manipulation

---
+++ src/DataStructures/Arrays.java
@@ -0,0 +1,23 @@
+package DataStructures;
+
+public class Arrays {
+    public static void main(String[] args){
+        //TODO- Declare a variable named colors of an array of String of length 5
+        String[] colors = new String[5];
+
+        //TODO- Fill the array with the requested colors
+        colors[0] = "red";
+        colors[1] = "yellow";
+        colors[2] = "orange";
+        colors[3] = "green";
+        colors[4] = "blue";
+
+        //TODO- Replace green with emerald in the array
+        colors[3] = "emerald";
+
+        //Print the contents of the array
+        for(String color:colors){
+            System.out.println(color);
+        }
+    }
+}
