d68a11f789b174b1973de02a66d13c96df1bf6e9
Paul Ogolla <paulotieno2@gmail.com>
2023-08-14 14:57:59 +0300

    recursion lesson

---
+++ src/recursion/Recursion.java
@@ -0,0 +1,13 @@
+package recursion;
+
+public class Recursion {
+    public static  int factorial(int n){
+        if(n==1) return 1;
+        else return n = n * factorial(n-1);
+    }
+
+    public static void main(String[]args){
+        System.out.println(factorial(20));
+    }
+
+}
