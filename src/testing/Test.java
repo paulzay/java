cb00c27af3f90052e7b458c975838fe480a885b7
Paul Ogolla <paulotieno2@gmail.com>
2023-08-15 22:43:43 +0300

    testing lesson

---
+++ src/testing/Test.java
@@ -0,0 +1,12 @@
+package testing;
+
+public class Test {
+
+    //TODO: Create two methods
+
+    public static void main(String[] args) {
+
+        //TODO: Print the output of these two methods in main
+        //TODO: Run the test methods for the two methods in the TryTest class
+    }
+}
---
+++ src/testing/TryTest.java
@@ -0,0 +1,5 @@
+package testing;
+
+public class TryTest {
+    //TODO: Create two test methods in here corresponding to the TryTest methods with the proper naming convention
+}
