12e92d3e1cc374a37f5615f7d02277874a21494f
Paul Ogolla <paulotieno2@gmail.com>
2023-08-17 12:15:26 +0300

    add new methods for completing tasks and use hashmap

--- src/project/MorningRoutine.java
+++ src/project/MorningRoutine.java
@@ -2,7 +2,18 @@ package project;
 
 public class MorningRoutine {
     public static void main(String[]args){
-        TodoList myList = new TodoList("Wednesday Grind");
-        myList.addTask("Wakeup");
+        TodoList myTodoList = new TodoList("Wednesday Grind");
+        myTodoList.addTask("Wake up");
+        myTodoList.addTask("Shower");
+        myTodoList.addTask("Have breakfast");
+        myTodoList.addTask("Go to work");
+        myTodoList.display();
+        myTodoList.markAsDone("Wake up");
+        myTodoList.markAsDone("My working day");
+        myTodoList.display();
+        myTodoList.removeTask("Have Breakfast");
+        myTodoList.display();
+        myTodoList.rename("Shower", "Take bath");
+        myTodoList.display();
     }
 }
--- src/project/TodoList.java
+++ src/project/TodoList.java
@@ -1,47 +1,61 @@
 package project;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TodoList {
-    private String title;
-    private ArrayList<String> tasks;
+    String title;
+    HashMap<String, Boolean> tasks;
 
     public TodoList(String title){
         this.title = title;
-        this.tasks = tasks;
+        this.tasks = new HashMap<>();
     }
 
-
     public void addTask(String taskName){
-        this.tasks.add(taskName);
-    };
+        this.tasks.put(taskName, false);
+    }
 
     public void display(){
         if(this.tasks.isEmpty()){
             System.out.println("Our Todo List for " + title + "is currently empty!");
         }else {
             System.out.println("Here is our Todo List for " + title);
-            for(int i=0;i<this.tasks.size();i++) {
-                System.out.println("Task " + i + " is " + this.tasks.get(i));
+            for(Map.Entry<String, Boolean> task : this.tasks.entrySet()) {
+                if (task.getValue()) {
+                    System.out.println("Task " + task.getKey() + " is complete");
+                }else {
+                    System.out.println("Task " + task.getKey() + " is not done");
+                }
             }
-        }
-
-    };
-
-    public  void remove(int i){
-        try{
-            this.tasks.remove(i);
-        }catch(IndexOutOfBoundsException e) {
-            System.out.println("Could not remove task, no task at this index!");
         }
     }
 
-    public void rename(int i, String newTask) {
-        try {
-            this.tasks.set(i,newTask);
+    public void removeTask(String taskName) {
+        if (this.tasks.containsKey(taskName)){
+            System.out.println("Removing " + taskName);
+            this.tasks.remove(taskName);
+        }else {
+            System.out.println("No such task!");
         }
-        catch (IndexOutOfBoundsException e) {
-            System.out.println("Could not rename task, no task at this index!");
+    }
+
+    public void rename(String oldTask, String newTask) {
+        System.out.println("Renaming " + oldTask + " to " + newTask);
+        if (this.tasks.containsKey(oldTask)){
+            this.tasks.put(newTask, this.tasks.get(oldTask));
+            this.tasks.remove(oldTask);
+        }else {
+            System.out.println("No such task!");
+        }
+    }
+
+    public void markAsDone(String taskName){
+        System.out.println("Marking " + taskName + " as done");
+        if(this.tasks.containsKey(taskName)){
+            this.tasks.put(taskName, true);
+        }else {
+            System.out.println("No such task found");
         }
     }
 
