2e65226d14e4cf510ac23b77762649e8c432ab10
Paul Ogolla <paulotieno2@gmail.com>
2023-08-24 21:20:50 +0300

    begin challenge

---
+++ src/Leetcode/lengthOfLastWord.java
@@ -0,0 +1,14 @@
+package Leetcode;
+
+public class lengthOfLastWord {
+//    Given a string s consisting of words and spaces, return the length of the last word in the string.
+//
+//    A word is a maximal substring consisting of non-space characters only.
+    public static void main(String[]args){
+        String s = "Hello World";
+        System.out.println(lengthOfLastWord(s));
+    }
+    public static int lengthOfLastWord(String s){
+        return 0;
+    }
+}
